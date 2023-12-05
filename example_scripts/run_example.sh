2d08f7ac6593815f33f56250976a7e3ed9f10575
Jason Chin <cschin@infoecho.net>
2023-12-05 14:35:27 -0800

    fix a typo

--- example_scripts/run_example.sh
+++ example_scripts/run_example.sh
@@ -1,5 +1,5 @@
 # generate the speficiation for a random 12x12 grid with random rewards
-python generate_spec_random.sh > test.json
+python generate_spec_random.py > test.json
 
 # train the model from scratch with batch size of 500 trajectories and 320 iteration 
 simple_2D_grid_gfn test.json test.1. -b 500 -o 1 -n 320 --save-all-batches
@@ -9,3 +9,5 @@ simple_2D_grid_gfn test.json test.1. -b 500 -o 1 -n 320 --save-all-batches
 # each batch is used for training 5 times
 simple_2D_grid_gfn test.json test.2. --model-file test.2.safetensors  -b 500 -o 5 -n 320 --save-all-batches
 
+
+# simple_2D_grid_gfn test3.json test3 -b 500 -o 1 -n 10000 --save-all-batches
